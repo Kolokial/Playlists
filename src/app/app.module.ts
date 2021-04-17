41608eb8f3b5a8c67f99e1ec5cb06c809a37140b
gary luck <koloqial@icloud.com>
2021-04-17 21:32:58 +0100

    initial commit, ran prettier.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HttpClientModule } from '@angular/common/http'
+import { PlaylistArtComponent } from './components/playlist-art/playlist-art.component'
+import { PlaylistsService } from './providers/playlists/playlists.service'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { PlaylistsModule } from './features/playlists/playlists.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, PlaylistArtComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        PlaylistsModule,
+    ],
+    providers: [PlaylistsService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
