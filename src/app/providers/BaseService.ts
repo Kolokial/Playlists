ded50f78ddcf6a1c579155b92cc70200650a6102
gary luck <koloqial@icloud.com>
2021-04-17 22:10:53 +0100

    added individual get for playlist.service. tidied up baseService.

--- src/app/providers/BaseService.ts
+++ src/app/providers/BaseService.ts
@@ -1,21 +1,21 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable, throwError } from 'rxjs'
-import { catchError, retry } from 'rxjs/operators'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 export class BaseService {
-    private _httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-        }),
+  private _httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private _http: HttpClient) {}
+
+  protected httpGet<T>(url: string): Observable<T> {
+    if (!url) {
+      return throwError('Please provide an URL.');
     }
 
-    constructor(private _http: HttpClient) {}
-
-    protected httpGet<T>(url: string): Observable<T> {
-        if (!url) {
-            return throwError('Please provide an URL.')
-        }
-
-        return this._http.get<T>(url, this._httpOptions)
-    }
+    return this._http.get<T>(url, this._httpOptions);
+  }
 }
--- src/app/providers/playlists/playlists.service.ts
+++ src/app/providers/playlists/playlists.service.ts
@@ -1,20 +1,26 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { BaseService } from '../BaseService'
-import { IFeaturedPlaylistsResponse } from './IPlaylists'
-import { Observable } from 'rxjs'
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseService } from '../BaseService';
+import { IFeaturedPlaylistsResponse, IPlaylistContent } from './IPlaylists';
+import { Observable } from 'rxjs';
+import { filter, mergeMap } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class PlaylistsService extends BaseService {
-    constructor(_http: HttpClient) {
-        super(_http)
-    }
+  constructor(_http: HttpClient) {
+    super(_http);
+  }
 
-    public getPlaylists(): Observable<IFeaturedPlaylistsResponse> {
-        return this.httpGet<IFeaturedPlaylistsResponse>(
-            'api/featured-playlists.json'
-        )
-    }
+  public getPlaylists(): Observable<IFeaturedPlaylistsResponse> {
+    return this.httpGet<IFeaturedPlaylistsResponse>('api/featured-playlists.json');
+  }
+
+  public getPlaylistContentById(id: string): Observable<IPlaylistContent> {
+    return this.getPlaylists().pipe(
+      mergeMap((playlistResponse) => playlistResponse.featuredPlaylists.content),
+      filter((playlist) => playlist.id === id)
+    );
+  }
 }
