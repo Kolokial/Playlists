d1dfe860451ea38c88d9dcbc6edc9f9cc0b5a009
gary luck <koloqial@icloud.com>
2021-04-17 23:59:32 +0100

    added home button. updated prettier.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PlaylistArtComponent } from './components/playlist-art/playlist-art.component';
+import { PlaylistArtComponent } from './features/playlist-art/playlist-art.component';
 import { PlaylistsComponent } from './features/playlists/playlists.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { HttpClientModule } from '@angular/common/http'
-import { PlaylistArtComponent } from './components/playlist-art/playlist-art.component'
-import { PlaylistsService } from './providers/playlists/playlists.service'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { PlaylistsModule } from './features/playlists/playlists.module'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PlaylistsService } from './providers/playlists/playlists.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlaylistsModule } from './features/playlists/playlists.module';
+import { PlaylistArtModule } from './features/playlist-art/playlist-art.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ErrorCardModule } from './components/error-card/error-card.module';
 
 @NgModule({
-    declarations: [AppComponent, PlaylistArtComponent],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        PlaylistsModule,
-    ],
-    providers: [PlaylistsService],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PlaylistsModule,
+    PlaylistArtModule,
+    MatButtonModule,
+    MatIconModule,
+    ErrorCardModule,
+  ],
+  providers: [PlaylistsService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
