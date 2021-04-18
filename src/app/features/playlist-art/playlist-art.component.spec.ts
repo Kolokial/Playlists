79655894650d6b303f55ad3a2bdd62149d608756
gary luck <koloqial@icloud.com>
2021-04-18 20:59:20 +0100

    added some tests for playlist-art and playlists.

--- src/app/features/playlist-art/playlist-art.component.spec.ts
+++ src/app/features/playlist-art/playlist-art.component.spec.ts
@@ -1,24 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-import { PlaylistArtComponent } from './playlist-art.component'
+import { PlaylistArtComponent } from './playlist-art.component';
 
 describe('PlaylistArtComponent', () => {
-    let component: PlaylistArtComponent
-    let fixture: ComponentFixture<PlaylistArtComponent>
+  let component: PlaylistArtComponent;
+  let fixture: ComponentFixture<PlaylistArtComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [PlaylistArtComponent],
-        }).compileComponents()
-    })
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PlaylistArtComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: 123 }),
+          },
+        },
+      ],
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(PlaylistArtComponent)
-        component = fixture.componentInstance
-        fixture.detectChanges()
-    })
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlaylistArtComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy()
-    })
-})
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/features/playlists/playlists.component.spec.ts
+++ src/app/features/playlists/playlists.component.spec.ts
@@ -1,24 +1,62 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import {
+  IFeaturedPlaylists,
+  IFeaturedPlaylistsResponse,
+  IPlaylistContent,
+} from 'src/app/providers/playlists/IPlaylists';
+import { PlaylistsService } from 'src/app/providers/playlists/playlists.service';
 
-import { PlaylistsComponent } from './playlists.component'
+import { PlaylistsComponent } from './playlists.component';
 
 describe('PlaylistsComponent', () => {
-    let component: PlaylistsComponent
-    let fixture: ComponentFixture<PlaylistsComponent>
+  let component: PlaylistsComponent;
+  let fixture: ComponentFixture<PlaylistsComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [PlaylistsComponent],
-        }).compileComponents()
-    })
+  let playlistContent = {
+    id: 'pl.test',
+    kind: 'myKind',
+    name: 'My Test playlist',
+    artwork: 'artworkUrl',
+    curator_name: 'Curatorrrr',
+  } as IPlaylistContent;
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(PlaylistsComponent)
-        component = fixture.componentInstance
-        fixture.detectChanges()
-    })
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PlaylistsComponent],
+      providers: [
+        {
+          provide: PlaylistsService,
+          useValue: {
+            getPlaylists: () =>
+              of({
+                featuredPlaylists: {
+                  content: [playlistContent] as IPlaylistContent[],
+                } as IFeaturedPlaylists,
+              } as IFeaturedPlaylistsResponse),
+          },
+        },
+      ],
+    }).compileComponents();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy()
-    })
-})
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlaylistsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('if the playlist component recieves data', () => {
+    it('it should show a list item', () => {
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('.playlist-name').textContent).toContain(playlistContent.name);
+      expect(compiled.querySelector('.playlist-curator').textContent).toContain(playlistContent.curator_name);
+      expect(compiled.querySelector('.artwork').src).toContain(playlistContent.artwork);
+    });
+  });
+});
